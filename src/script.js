2ca7ffb899a3c7a0b2d0838ac913993fc7be81bc
MoyyyL <moyyyl0526@gmail.com>
2025-07-30 07:13:48 -0600

    its working already

--- src/script.js
+++ src/script.js
@@ -3,7 +3,8 @@ import { gettingWeather } from "./weatherAPI";
 
 class domWorker {
     constructor() {
-        this.start = this.renderClimate();
+        this.start = this.ownData("london")
+        this.startListeners = this.addListeners();
     }
 
     addListeners() {
@@ -13,18 +14,46 @@ class domWorker {
         form.addEventListener("submit", (e) => {
             e.preventDefault();
             const place = location.value.trim();
-            
-            const info = gettingWeather(place); //todo No olvidar agregar la opcion de cambiar entre "metric" y "us"
-            info.then(data => console.log(data))
+            this.ownData(place);
         })
     }
 
-    renderClimate() {
-        const initialWeather = gettingWeather();
+    async ownData(place) {
+        const data = await gettingWeather(place);
+        console.log(data)
 
+        this.renderClimate(data.address, data.icon, data.max, data.min, data.temp);
+    }
+
+    renderClimate(address, dataIcon, maxTemp, minTemp, currentTemp) {
         const display = document.querySelector(".weather__display");
+        display.innerHTML = "";
         
-        
+        const icon = document.createElement("img");
+        icon.classList.add("image");
+        icon.src = dataIcon;
+
+        const temp = document.createElement("p");
+        temp.classList.add("temperature");
+        temp.textContent = `${currentTemp}°C`
+
+        const place = document.createElement("p");
+        place.classList.add("place");
+        place.textContent = `${address}`;
+
+        const min = document.createElement("p");
+        min.classList.add("min");
+        min.textContent = `min: ${minTemp}°C`;
+
+        const max = document.createElement("p");
+        max.classList.add("max");
+        max.textContent = `max: ${maxTemp}°C`;
+
+        display.appendChild(icon);
+        display.appendChild(temp);
+        display.appendChild(place);
+        display.appendChild(min);
+        display.appendChild(max);
     }
 }
 
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,34 +1,37 @@
 async function gettingWeather(place = "london,Uk", unit = "metric") {
     const apiInfo = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/today?unitGroup=${unit}&key=2VHBYPYNMEL2T5NVPRCCKW4EF&include=days,current&elements=datetime,tempmax,tempmin,temp,conditions`,
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/today?unitGroup=${unit}&key=2VHBYPYNMEL2T5NVPRCCKW4EF&iconSet=icons1&include=days,current&elements=tempmax,tempmin,temp,icon`,
         { mode: "cors" }
     );
     
     const weatherInfo = await apiInfo.json();
-
     return discerning(weatherInfo)
 }
 
 function discerning(data) {
     const today = data.days[0];
-    const currentConditions = data.currentConditions;
-    const address = data.resolvedAddress;
-
-    const temp = currentConditions.temp;
-    const conditions = currentConditions.conditions;
-
     const maxTemp = today.tempmax;
     const minTemp = today.tempmin;
-    const date = today.datetime;
+    
+    const currentConditions = data.currentConditions;
+    const icon = getIconURL(currentConditions.icon);
+
+    const temp = currentConditions.temp;
+    
+    const address = data.resolvedAddress;
 
     return {
-        date: date,
-        address: address,
         temp: temp,
         max: maxTemp,
         min: minTemp,
-        conditions: conditions,
+        icon: icon,
+        address: address,
     }
 }
+
+function getIconURL(name) {
+    return `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/refs/heads/main/SVG/2nd%20Set%20-%20Color/${name}.svg`
+}
+
 
 export {gettingWeather}
